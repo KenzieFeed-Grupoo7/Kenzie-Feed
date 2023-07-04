f93e7de9a4793ac305b5096b7695c851a746e248
João Augusto <jaugustovpires@gmail.com>
2023-07-04 12:11:08 -0300

    feat: login funcional/ pasta dashboard inicio

--- src/Components/Header/style.ts
+++ src/Components/Header/style.ts
@@ -1,26 +1,24 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    padding: 1rem;
+  padding: 1rem;
 
-    width: 100%;
-    height: 7rem;
-    
-
-`
+  width: 100%;
+  height: 7rem;
+`;
 
 export const StyledUser = styled.div`
-    background-color: var(--color-blue);
-    border-radius: 50%;
+  background-color: var(--color-blue);
+  border-radius: 50%;
 
-    width: 43px;
-    height: 43px;
+  width: 43px;
+  height: 43px;
 
-    font-family: var(--font-secondary);
-    font-size: .875rem;
-    font-weight: 700;
-`
+  font-family: var(--font-secondary);
+  font-size: 0.875rem;
+  font-weight: 700;
+`;
