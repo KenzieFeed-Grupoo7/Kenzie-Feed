a57a6734627e5347502fcd0690450e18f6a6fa81
gstvcampos <gustavo.bmc76@gmail.com>
2023-07-06 18:48:25 -0300

    feat: estilo modal

--- src/Pages/Dashboard/CreateNewsModal/index.tsx
+++ src/Pages/Dashboard/CreateNewsModal/index.tsx
@@ -3,6 +3,7 @@ import { CreateNewsForm } from "./CreateNewsForm/index.tsx";
 import { NewsContext } from "../../../Providers/NewsContexts/NewsContexts.js";
 import { StyledModalContainer } from "./index.ts";
 import close from "../../../Assets/close.svg"
+import { StyledTitleTwo } from "../../../Styles/typography.ts";
 
 export const CreateNewsModal = () => {
   const { closeModal } = useContext(NewsContext);
@@ -11,7 +12,7 @@ export const CreateNewsModal = () => {
     <StyledModalContainer>
       <div className="modal__content">
         <div className="content__header">
-          <h2>Novo Post</h2>
+          <StyledTitleTwo>Novo Post</StyledTitleTwo>
           <button onClick={closeModal}>
             <img src={close} alt="botao fechar" />
           </button>
