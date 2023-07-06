487c498e1c7e0148c95c5fa9e5f77ce0fe769ab4
gstvcampos <gustavo.bmc76@gmail.com>
2023-07-06 19:12:32 -0300

    fix: conflitos

--- src/Pages/Dashboard/UserNews/UserNewsCard/index.tsx
+++ src/Pages/Dashboard/UserNews/UserNewsCard/index.tsx
@@ -3,6 +3,8 @@ import { INews } from "../../../../Providers/NewsContexts/@types.ts";
 import { StyledTitleThree } from "../../../../Styles/typography.ts";
 import { StyledCard } from "./index.ts";
 import { NewsContext } from "../../../../Providers/NewsContexts/NewsContexts.tsx";
+import edit from "../../../../Assets/edit.svg";
+import deleteImg from "../../../../Assets/delete.svg";
 
 interface IUserNewsCardProps {
   news: INews;
@@ -13,11 +15,18 @@ export const UserNewsCard = ({ news }: IUserNewsCardProps) => {
 
   return (
     <StyledCard>
-      <img src={news.image} alt="imagem referente a notícia" />
-      <p>{news.description}</p>
-      <div>
-        <button>Editar</button>
-        <button onClick={() => deletePost(Number(news.id))}>Excluir</button>
+      <div className="img__container">
+        <img src={news.image} alt="imagem referente a notícia" />
+        <StyledTitleThree>{news.description}</StyledTitleThree>
+      </div>
+
+      <div className="button__container">
+        <button>
+          <img src={edit} alt="imagem editar" />
+        </button>
+        <button onClick={() => deletePost(Number(news.id))}>
+          <img src={deleteImg} alt="imagem editar" />
+        </button>
       </div>
     </StyledCard>
   );
